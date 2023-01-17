aba12a5c4503d8d6f6f09730cb4d65818880b0a4
Eduardo Hoefel <eduardo@eduardo.kpn>
2023-01-17 20:30:12 +0100

    added footer and header

--- zmdocs.config.cjs
+++ zmdocs.config.cjs
@@ -1,12 +1,81 @@
 module.exports = {
-  head: '',               // HTML string added into start of `<head>`
-  header: '',             // HTML string added to start of `<body>`
-  footer: '',             // HTML string added to end of `<body>`
-  links: [                
-    { title: 'Overview', href: '/horror-rpg-framework/', dir: 'docs' }, // Array of navigation links
-    { title: 'Foo', href: '/horror-rpg-framework/foo/', dir: 'docs/foo' },
-    { title: 'Bar', href: '/horror-rpg-framework/bar/', dir: 'docs/bar' },
-    { title: 'Menu Item', href: '/horror-rpg-framework/menu-item/', dir: 'docs/menu-item' }
+  head: `<link rel="icon" type="image/x-icon" href="/zero-md/favicon.ico">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Delius&text=Horror-RPG-Framework&display=swap">`,
+  header: `<header style="display:flex;flex-direction:column;align-items:center;font-size:3.75rem;margin-top:0.75rem;">
+  <h1 style="font-family:'Delius'; margin:1.5rem">Horror RPG Framework</h1>
+</header>`,
+  footer: `<footer style="text-align:center;font-size:0.875rem;margin-top:4rem;margin-bottom:0.75rem">
+  <div style="margin-top:0.75rem;margin-bottom:1.5rem;display:flex;justify-content:center;gap:1rem">
+  <a href="https://github.com/ehoefel/horror-rpg-framework">
+    <img src="https://img.shields.io/badge/CONTRIBUTE-ehoefel%2Fhorror--rpg--framework-green?style=for-the-badge&logo=github">
+  </a>
+    <img src="https://img.shields.io/github/license/ehoefel/horror-rpg-framework?style=for-the-badge&logo=github">
+  </div>
+  <p style="font-size:0.75rem;color:#6b7280;">
+    SITE GENERATED WITH <a href="https://github.com/zerodevx/zero-md-docs">ZERO-MD-DOCS</a>
+  </p>
+</footer>`,
+  links: [
+    {
+      title: 'Overview',
+      href: '/horror-rpg-framework/',
+      dir: 'docs'
+    },
+    {
+      title: 'Readings',
+      href: '/horror-rpg-framework/readings/',
+      dir: 'docs/readings'
+    },
+    {
+      title: 'RPG Systems',
+      href: '/horror-rpg-framework/rpg-systems/',
+      dir: 'docs/rpg-systems'
+    },
+    {
+      title: 'Story design',
+      href: '/horror-rpg-framework/story-design/',
+      dir: 'docs/story-design'
+    },
+    {
+      title: 'Worldbuilding',
+      href: '/horror-rpg-framework/worldbuilding/',
+      dir: 'docs/worldbuilding'
+    },
+    {
+      title: 'Character design',
+      href: '/horror-rpg-framework/character-design/',
+      dir: 'docs/character-design'
+    },
+    {
+      title: 'Storytelling',
+      href: '/horror-rpg-framework/storytelling/',
+      dir: 'docs/storytelling'
+    },
+    {
+      title: 'Play',
+      href: '/horror-rpg-framework/play/',
+      dir: 'docs/play'
+    },
+    {
+      title: 'Environment & mood',
+      href: '/horror-rpg-framework/environment-and-mood/',
+      dir: 'docs/environment-and-mood'
+    },
+    {
+      title: 'Player Engagement',
+      href: '/horror-rpg-framework/player-engagement/',
+      dir: 'docs/player-engagement'
+    },
+    {
+      title: 'Final notes',
+      href: '/horror-rpg-framework/final-notes/',
+      dir: 'docs/final-notes'
+    },
+    {
+      title: 'References & Resources',
+      href: '/horror-rpg-framework/references-and-resources/',
+      dir: 'docs/references-and-resources'
+    }
   ],
   _template: 'default',   // Use the `default` template
   _title: ''              // Dummy placeholder (required)
