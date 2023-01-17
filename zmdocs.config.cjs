36f6faad37abb18a1cfeed82361d6a4fd651473f
Eduardo Hoefel <eduardo@eduardo.kpn>
2023-01-17 22:58:51 +0100

    Renamed to fearpg

--- zmdocs.config.cjs
+++ zmdocs.config.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   head: `<link rel="icon" type="image/x-icon" href="/zero-md/favicon.ico">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Delius&text=Horror-RPG-Framework&display=swap">`,
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Delius&text=fearpg&display=swap">`,
   header: `<header style="display:flex;flex-direction:column;align-items:center;font-size:3.75rem;margin-top:0.75rem;">
-  <h1 style="font-family:'Delius'; margin:1.5rem">Horror RPG Framework</h1>
+  <h1 style="font-family:'Delius'; margin:1.5rem">fearpg</h1>
 </header>`,
   footer: `<footer style="text-align:center;font-size:0.875rem;margin-top:4rem;margin-bottom:0.75rem">
   <div style="margin-top:0.75rem;margin-bottom:1.5rem;display:flex;justify-content:center;gap:1rem">
-  <a href="https://github.com/ehoefel/horror-rpg-framework">
-    <img src="https://img.shields.io/badge/CONTRIBUTE-ehoefel%2Fhorror--rpg--framework-green?style=for-the-badge&logo=github">
+  <a href="https://github.com/ehoefel/fearpg">
+    <img src="https://img.shields.io/badge/CONTRIBUTE-ehoefel%2Ffearpg-green?style=for-the-badge&logo=github">
   </a>
-    <img src="https://img.shields.io/github/license/ehoefel/horror-rpg-framework?style=for-the-badge&logo=github">
+    <img src="https://img.shields.io/github/license/ehoefel/fearpg?style=for-the-badge&logo=github">
   </div>
   <p style="font-size:0.75rem;color:#6b7280;">
     SITE GENERATED WITH <a href="https://github.com/zerodevx/zero-md-docs">ZERO-MD-DOCS</a>
@@ -18,62 +18,62 @@ module.exports = {
   links: [
     {
       title: 'Overview',
-      href: '/horror-rpg-framework/',
+      href: '/fearpg/',
       dir: 'docs'
     },
     {
       title: 'Inspirations',
-      href: '/horror-rpg-framework/inspirations/',
+      href: '/fearpg/inspirations/',
       dir: 'docs/inspirations'
     },
     {
       title: 'RPG Systems',
-      href: '/horror-rpg-framework/rpg-systems/',
+      href: '/fearpg/rpg-systems/',
       dir: 'docs/rpg-systems'
     },
     {
       title: 'Story design',
-      href: '/horror-rpg-framework/story-design/',
+      href: '/fearpg/story-design/',
       dir: 'docs/story-design'
     },
     {
       title: 'Worldbuilding',
-      href: '/horror-rpg-framework/worldbuilding/',
+      href: '/fearpg/worldbuilding/',
       dir: 'docs/worldbuilding'
     },
     {
       title: 'Character design',
-      href: '/horror-rpg-framework/character-design/',
+      href: '/fearpg/character-design/',
       dir: 'docs/character-design'
     },
     {
       title: 'Storytelling',
-      href: '/horror-rpg-framework/storytelling/',
+      href: '/fearpg/storytelling/',
       dir: 'docs/storytelling'
     },
     {
       title: 'Play',
-      href: '/horror-rpg-framework/play/',
+      href: '/fearpg/play/',
       dir: 'docs/play'
     },
     {
       title: 'Environment & mood',
-      href: '/horror-rpg-framework/environment-and-mood/',
+      href: '/fearpg/environment-and-mood/',
       dir: 'docs/environment-and-mood'
     },
     {
       title: 'Player Engagement',
-      href: '/horror-rpg-framework/player-engagement/',
+      href: '/fearpg/player-engagement/',
       dir: 'docs/player-engagement'
     },
     {
       title: 'Final notes',
-      href: '/horror-rpg-framework/final-notes/',
+      href: '/fearpg/final-notes/',
       dir: 'docs/final-notes'
     },
     {
       title: 'References & Resources',
-      href: '/horror-rpg-framework/references-and-resources/',
+      href: '/fearpg/references-and-resources/',
       dir: 'docs/references-and-resources'
     }
   ],
